f3b26b781041cf8a25d8eaaa58c62b158852aa12
reduviidae <wiljago@gmail.com>
2019-01-29 13:51:25 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,14 @@ const codes = [
 
 function init() {
   // your code here
+  let counter = 0;
+  document.body.addEventListener("keydown", e => {
+    (e.key === codes[counter]) ? counter ++ : counter = 0;
+    if (counter === codes.length){
+      alert("test");
+      console.log("alert fired");
+      counter = 0;
+      console.log(counter);
+    }
+  })
 }
